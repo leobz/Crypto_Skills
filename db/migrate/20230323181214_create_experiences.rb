e27820633ca8844a454110ea3928d10d863fafe1
Fecox99 <franxsufix@gmail.com>
2023-03-23 15:15:51 -0300

    Creacion de clases Education y Experience

---
+++ db/migrate/20230323181214_create_experiences.rb
@@ -0,0 +1,17 @@
+class CreateExperiences < ActiveRecord::Migration[7.0]
+  def change
+    create_table :experiences do |t|
+      t.string :title
+      t.string :company_name
+      t.string :location
+      t.boolean :currently_working
+      t.date :star_date
+      t.date :end_date
+      t.integer :industry
+      t.text :description
+      t.string :media_links
+
+      t.timestamps
+    end
+  end
+end
