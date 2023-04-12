c1625d9905108d3f4794eaf6a52bb3b808ff0750
Leonel Bazán <leo.bazan.x@gmail.com>
2023-04-12 16:33:28 -0300

    Add ActiveStorage and Add company_logo attachment to Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,6 @@ class Job < ApplicationRecord
     # Uncomment if you use companies table
     # belongs_to :company
     enum location_mode: [ :REMOTE, :ON_SITE, :HYBRID]
+
+    has_one_attached :company_logo
 end
