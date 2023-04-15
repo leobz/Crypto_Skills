f154dd7c8e0d6fa12cfc1ddc158257d62990092c
Leonel Bazán <leo.bazan.x@gmail.com>
2023-04-15 20:54:41 -0300

    Job: :description fix breaklines renderization

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: %i[ show edit update destroy ]
+  before_action :set_job, :format_description, only: %i[ show edit update destroy ]
 
   # GET /jobs or /jobs.json
   def index
@@ -66,5 +66,12 @@ class JobsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def job_params
       params.require(:job).permit(:title, :description, :website, :company, :company_logo, :location, :location_mode, :salary, :category)
+    end
+
+    def format_description
+      # Convert line breaks of :description field into <br> tags to render correct view
+      if params[:job].present? && params[:job][:description].present?
+        params[:job][:description] = ActionController::Base.helpers.simple_format(params[:job][:description])
+      end
     end
 end
