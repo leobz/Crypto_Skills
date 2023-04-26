bf700128f69285bbd9790dd555bc14e9f07aa87d
Leonel Bazán <leo.bazan.x@gmail.com>
2023-04-25 22:21:01 -0300

    Add Invoice model/migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_230137) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_171123) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_19_230137) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -74,23 +74,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_19_230137) do
     t.string "media_links", default: "--- []\n"
   end
 
+# Could not dump table "invoices" because of following StandardError
+#   Unknown type 'uuid' for column 'id'
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.string "website"
-    t.integer "company_id"
+    t.string "company"
+    t.binary "company_logo"
     t.string "location"
     t.integer "location_mode"
-    t.date "date"
     t.integer "salary"
     t.integer "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "company"
-    t.index ["company_id"], name: "index_jobs_on_company_id"
+    t.boolean "published", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "jobs", "companies"
+  add_foreign_key "invoices", "jobs", column: "job_id"
 end
