cd0e5130c67c6dfc65981c8347ff18cd90282b25
Leonel Bazán <leo.bazan.x@gmail.com>
2023-05-01 21:15:17 -0300

    Add job card preview before publish Job

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,41 +1,32 @@
 module JobsHelper
-  def job_card(job)
-    content_tag(:div, class: 'card mb-3') do
-      content_tag(:div, class: 'row g-0 align-items-center') do
-        # Image
-        content_tag(:div, class: 'col-2 d-flex justify-content-center') do
-          image_tag('icons/company-bbb.png')
+  def job_card(job, full_size = nil)
+    content_tag(:div, class: "card", style: "" + (full_size ? 'transform: scale(1);' : '')) do
+      content_tag(:div, class: "row g-0 align-items-center") do
+        #***************************** Image *****************************
+        content_tag(:div, class: "col-2 d-flex justify-content-center") do
+          image_tag(job.company_logo, style: "width: 50px; height: 50px;")
         end +
-
-          # Body
-          content_tag(:div, class: 'col-10') do
-            content_tag(:div, class: 'card-body') do
-              # Title
-              content_tag(:h5, class: 'card-title') do
-                job.title
-              end +
-
-                # Subtitle
-                content_tag(:h6, class: 'card-subtitle mb-2 text-body-secondary') do
-                  job.company
-                end +
-
-                # Location
-                content_tag(:p, class: 'card-text', style: 'line-height: 1;') do
-                  concat content_tag(:small, job.location, class: 'text-body-secondary')
-                  concat tag.br(class: 'small')
-                  concat content_tag(:small, job.location_mode, class: 'text-body-secondary')
-                end +
-
-                # Date and Salary
-                content_tag(:span, class: 'card-text', style: 'float: left;') do
-                  content_tag(:small, job.created_at, class: 'text-body-secondary')
-                end +
-                content_tag(:span, class: 'card-text', style: 'float: right;') do
-                  content_tag(:small, "$#{job.salary}", class: 'text-body-secondary')
-                end
+        #***************************** Body ******************************
+        content_tag(:div, class: "col-10") do
+          content_tag(:div, class: "card-body") do
+            #***************************** Title *************************
+            content_tag(:h5, job.title, class: 'card-title') +
+            #***************************** Subtitle **********************
+            content_tag(:h6, job.company, class: 'card-subtitle mb-2 text-body-secondary') +
+            #***************************** Secondary *********************
+            content_tag(:p, class: 'card-text', style: 'line-height: 1;') do
+              concat content_tag(:small, job.location, class: 'text-body-secondary')
+              concat tag.br(class: 'small')
+              concat content_tag(:small, job.location_mode, class: 'text-body-secondary')
+            end +
+            content_tag(:span, class: 'card-text', style: 'float: left;') do
+              content_tag(:small, job.created_at, class: 'text-body-secondary')
+            end +
+            content_tag(:span, class: 'card-text', style: 'float: right;') do
+              content_tag(:small, "$#{job.salary ? job.salary : "------"}", class: 'text-body-secondary')
             end
           end
+        end
       end
     end
   end
