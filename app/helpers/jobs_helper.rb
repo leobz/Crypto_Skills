068dcc3d627d93a5b8dfd5c26e164933d4acdad1
Leo Bazan <leo.bazan.x@gmail.com>
2023-06-13 00:01:02 -0300

    UI changes

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -3,24 +3,30 @@ module JobsHelper
     content_tag(:div, class: "card card-index", style: "margin-bottom: 0.5%;" + (full_size ? 'transform: scale(1);' : '')) do
       content_tag(:div, class: "row g-0 align-items-center") do
         #***************************** Image *****************************
-        content_tag(:div, class: "col-2 d-flex justify-content-center") do
-          image_tag(job.company_logo, class: "logo-fluid")
+        content_tag(:div, class: "col-2 d-flex justify-content-right") do
+          image_tag(job.company_logo, class: "logo-fluid", style: "padding-left:15px;")
         end +
         #***************************** Body ******************************
         content_tag(:div, class: "col-10") do
           content_tag(:div, class: "card-body preview-card") do
             #***************************** Title *************************
-            content_tag(:h5, job.title, class: 'card-title') +
+            content_tag(:h4, job.title, class: 'card-title') +
             #***************************** Subtitle **********************
-            content_tag(:h6, job.company, class: 'card-subtitle mb-2 text-body-secondary') +
+            content_tag(:h6, job.company, class: 'card-subtitle mb-2') +
             #***************************** Secondary *********************
-            content_tag(:p, class: 'card-text', style: 'line-height: 1;') do
-              concat content_tag(:small, job.location, class: 'text-body-secondary')
-              concat tag.br(class: 'small')
-              concat content_tag(:small, job.location_mode, class: 'text-body-secondary')
-            end +
-            content_tag(:span, class: 'card-text', style: 'float: left;') do
-              content_tag(:small, job.created_at, class: 'text-body-secondary')
+            content_tag(:div, class: "info-container") do
+              concat(content_tag(:div, class: "info-item") do
+                concat tag.i(class: "fa fa-map-marker")
+                concat content_tag(:span, ".Bellingham, WA")
+              end)
+              concat(content_tag(:div, class: "info-item") do
+                concat tag.i(class: "fa fa-clock-o", 'aria-hidden': true)
+                concat(content_tag(:span, ".Freelance / Contract"))
+              end)
+              concat(content_tag(:div, class: "info-item") do
+                concat tag.i(class: "fa fa-wifi", 'aria-hidden': true)
+                concat(content_tag(:span, ".Remote OK"))
+              end)
             end +
             content_tag(:span, class: 'card-text', style: 'float: right;') do
               content_tag(:small, "$#{job.salary ? job.salary : "------"}", class: 'text-body-secondary')
