135e4af9ee7bb6f999f3de15b3e12cae909082ac
Leo Bazan <leo.bazan.x@gmail.com>
2023-11-27 01:50:37 -0300

    Fix Boostrap dropdown menu disabling Turbo
    More info:
    - Bug: https://stackoverflow.com/questions/71216446/bootstrap-5-1-not-working-in-jsbundling-rails-7/72553402#72553402
    - Replace "turbo_method: :delete" by "method: :delete": https://stackoverflow.com/questions/70474422/rails-7-link-to-with-method-delete-still-performs-get-request

--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,10 @@
 rails db:create
 rails db:migrate
 rails db:seed
+echo "Cleaning Cache..."
+rake assets:clean
+rake assets:clobber
+rake tmp:clear
 echo "Running Sidekiq in the background..."
 REDIS_URL="redis://127.0.0.1:6379/0" bundle exec sidekiq -e development -C config/sidekiq.yml &
-rails server -b 0.0.0.0
+bin/dev
